07b2156e6ed7a4b804d38133cc3f06557b86758c
Ricardo Maurici <ricardo.maurici@hotmail.com>
2014-11-26 16:36:03 -0200

    registro de recebimento dos docs do aluno

--- admin/paginas/recebimentoDocumentoAluno.php
+++ admin/paginas/recebimentoDocumentoAluno.php
@@ -3,6 +3,18 @@ require '../includes/header.html';
 require '../includes/menuAdmin.php';
 include('../../conecta.php');
 
+if(isset($_GET['altera'])){
+	$idDoc = $_GET['idDocumentacao'];
+	$idMat = $_GET['idMatricula'];
+
+	$s = "UPDATE `matricula_doc` SET `entregue`= 1 WHERE `idMatricula` = $idMat AND `idDocumentacao` = $idDoc";
+	$r = mysql_query($s);
+	if(!$r){
+		echo '<script>alert("'.mysql_error().'")</script>';
+	}else{
+		echo '<script>alert("Recebimento registrado com sucesso!")</script>';
+	}
+}
 ?>
 
 <div class="panel panel-default col-md-10 col-md-offset-1">
@@ -33,23 +45,28 @@ include('../../conecta.php');
     while($row=mysql_fetch_array($result)){
     	if($row['idMatricula'] > 0){
     		$idM = $row['idMatricula'];
-    		$query = "SELECT matRecebido FROM `Matricula` WHERE `idMatricula` = $idM AND `desistente` IS NULL ";
+    		$query = "SELECT md.`entregue`, d.`tipoDocumento`, md.`idDocumentacao`  FROM `matricula_doc` md join `Documentacao` d on md.`idDocumentacao`=d.`idDocumentacao` WHERE `idMatricula` = $idM";
     		$rquery = mysql_query($query);
-    		$x = mysql_fetch_array($rquery);
-    		if($x[0]){
-    			$recebido = 'checked="checked"';
-   				$botao = 'disabled="disabled"';
-   			}
+    		while($x=mysql_fetch_array($rquery)){
+    			if($x['entregue']){
+    				$recebido = 'checked="checked"';
+   					$botao = 'disabled="disabled"';
+   				}else{
+   					$recebido = '';
+   					$botao = '';
+   				}
+   				$pmts = '?idMatricula='.$idM.'&idDocumentacao='.$x['idDocumentacao'].'&altera=true';
 ?>    	
 	            	<tr> <!--Alimenta Banco de Dados/Se já recebeu desmarca botão-->
 		                <td><input type="checkbox" class="checkthis" disabled="disabled" <?php echo $recebido; ?> /></td>
 		                <td><?php echo $row['idMatricula']; ?></td>
 		                <td><?php echo $row['nomeCandidato']; ?></td>
 		                <td><?php echo $row['cpf']; ?></td>
-		                <td>Comprovante de residência</td>
-		              	<td><p><a href="registroEntregaMaterialAluno.php" class="btn btn-warning btn-xs" <?php echo $botao; ?>>Sim</a></p></td>
+		                <td><?php echo $x['tipoDocumento']; ?></td>
+		              	<td><p><a href="recebimentoDocumentoAluno.php<?php echo $pmts; ?>" class="btn btn-warning btn-xs" <?php echo $botao; ?>>Sim</a></p></td>
 		            </tr>
 <?php
+			}
 		}
 
 	}
