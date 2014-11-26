a7fa2da297de72891bceb66d3dc73c9222ffa5db
Ricardo Maurici <ricardo.maurici@hotmail.com>
2014-11-25 22:24:09 -0200

    concluido gerencia de turma

--- admin/paginas/deletarTurma.php
+++ admin/paginas/deletarTurma.php
@@ -8,10 +8,18 @@ require '../includes/menuAdmin.php';
     <h3 class="panel-title">Exclusão de Turma</h3>
   </div>
   <div class="panel-body">
-    <form class="form-horizontal" role="form">
+    <form class="form-horizontal" role="form" method="post" action="gerenciaTurma.php">
+<?php
+  echo '<input type="hidden" name="idTurma" value='.$_GET['turma'].'>';
+  echo '<input type="hidden" name="idPessoa" value='.$_GET['professor'].'>';
+  echo '<input type="hidden" name="idDisciplina" value='.$_GET['disciplina'].'>';
+  echo '<input type="hidden" name="idHorario" value='.$_GET['horario'].'>';
+  echo '<input type="hidden" name="deletar">';
+
+?>
       </div>
         <div class="alert alert-warning" style="width: 500px; margin-left:350px;"><p>Você tem certeza que deseja excluir esse cadastro? </p> 
-          <a href="gerenciaTurma.php" style="width: 80px; margin-left: 280px; margin-top: 20px;" class="btn btn-danger">Sim</a>
+          <button  type="submit" style="width: 80px; margin-left: 280px; margin-top: 20px;" class="btn btn-danger">Sim</button>
           <a href="gerenciaTurma.php" style="width: 80px; margin-left: 20px; margin-top: 20px;" class="btn btn-warning">Não</a>
         </div>
     </form>
--- admin/paginas/gerenciaTurma.php
+++ admin/paginas/gerenciaTurma.php
@@ -20,7 +20,55 @@ if(isset($_POST['cadastrar'])){
 			}
 		}
 	}
+}elseif (isset($_POST['alterar'])) {
+	$idCurso = $_POST['idCurso'];
+	$idSala = $_POST['idSala'];
+	$idTurma = $_POST['idTurma'];
+
+	for($i = 1; $i <= 10; $i++){
+		if($_POST[$i] != 'Selecione aqui'){
+			$p = explode(',',$_POST[$i]);
+
+			$q = "SELECT * FROM `Turma` WHERE `idTurma` = $idTurma AND `idPessoa` = $p[0] AND `idDisciplina` = $p[1]  AND `idHorario` = $p[2]";
+			$checkq = mysql_query($q);
+			$rowsq = mysql_num_rows($checkq);
+
+			if($rowsq == 0){
+				$sql = "INSERT INTO `Turma`(`idTurma`, `idPessoa`, `idDisciplina`, `idHorario`, `idSala`, `idCurso`) VALUES 
+				($idTurma,$p[0],$p[1],$p[2],$idSala,$idCurso)";
+				$result=mysql_query($sql);
+				if(!$result){
+					echo '<meta http-equiv="refresh" content="0;url=cadastroTurma.php">';
+					echo '<script>alert("'.mysql_error().'")</script>';
+				}
+			}else{
+
+				$xql = "UPDATE `Turma` SET `idSala`= $idSala,`idCurso`= $idCurso  WHERE `idTurma` = $idTurma AND `idPessoa` = $p[0] AND
+				 `idDisciplina` = $p[1] AND `idHorario` = $p[2]";
+				$result=mysql_query($xql);
+				if(!$result){
+					echo '<meta http-equiv="refresh" content="0;url=cadastroTurma.php">';
+					echo '<script>alert("'.mysql_error().'")</script>';
+				}
+			}
+		}
+	}
+}elseif (isset($_POST['deletar'])) {
+	$turm = $_POST['idTurma'];
+	$pes = $_POST['idPessoa'];
+	$dis = $_POST['idDisciplina'];
+	$hor = $_POST['idHorario'];
+
+	$query = "DELETE FROM `Turma` WHERE `idTurma` = $turm AND idPessoa = $pes AND idDisciplina = $dis AND idHorario = $hor";
+	$results=mysql_query($query);
+	if(!$results){
+		echo '<script>alert("'.mysql_error().'")</script>';
+	}else{
+		echo '<script>alert("Exclusão realizada com sucesso!")</script>';
+	} 
+	
 }
+
 ?>
 
 <div class="panel panel-default col-md-10 col-md-offset-1">
@@ -53,8 +101,9 @@ if(isset($_POST['cadastrar'])){
 				$q = "SELECT concat(`tipo`,' - ',`turno`,' ',year(`dtInicioInscricao`)) c FROM `Curso` where idCurso = {$row['idCurso']}";
 				$r = mysql_fetch_array(mysql_query($q));
 				echo '<td>'.$r['c'].'</td>';
-		        echo '<td><p><a href="alterarTurma.php?turma='.$row['idTurma'].'&pessoa='.$row['idPessoa'].'&disciplina='.$row['idDisciplina'].'&horario='.$row['idHorario'].'" class="btn btn-warning btn-xs">Alterar</a>';
-		        echo '<td><p><a href="deletarTurma.php?turma='.$row['idTurma'].'&pessoa='.$row['idPessoa'].'&disciplina='.$row['idDisciplina'].'&horario='.$row['idHorario'].'" class="btn btn-danger btn-xs">Excluir</a>';
+				$parametros = 'turma='.$row['idTurma'].'&professor='.$row['idPessoa'].'&disciplina='.$row['idDisciplina'].'&horario='.$row['idHorario'];
+		        echo '<td><p><a href="alterarTurma.php?turma='.$row['idTurma'].'" class="btn btn-warning btn-xs">Alterar</a>';
+		        echo '<td><p><a href="deletarTurma.php?'.$parametros.'" class="btn btn-danger btn-xs">Excluir</a>';
 		        echo '</tr>'; 
 		    }         
 ?>			              	 
